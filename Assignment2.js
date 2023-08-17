2b16716841f558947f08d89abe9729d271ae2d9c
kaushalyad321 <140784503+kaushalyad321@users.noreply.github.com>
2023-08-17 17:15:01 +0530

    Assignment2.js updated

--- Assignment2.js
+++ Assignment2.js
@@ -1,9 +1,9 @@
-//1. Write a program to give a random output between 1 to 10 (AC: 2 simultaneous answers could not be same)
-console.log(Math.floor(Math.random()*10+1)) ;
+//1. Write a program to give a random output between 1 to 10 
+console.log(Math.floor(Math.random()*10+1)) ;// used Inbuilt random function to generate number between [0,1) and multiplying with 10 and adding value 1 .
 //2. Write a program to take input(age, name, phone no.) from user and display it
-let name=prompt("please enter your name") ;
-let age=prompt("please enter your age") ;
-let phone_no= prompt("please enter your phone No") ;
+let name=prompt("please enter your name") ;//taking name as an input from user
+let age=prompt("please enter your age") ;//taking age as an input from user
+let phone_no= prompt("please enter your phone No") ;//taking phone no as an input from user
 console.log(`Hi! ${name} your age is ${age} and your phone no is ${phone_no}`) ;
 //3. Write a function expression that takes in another function as an argument
 
@@ -29,11 +29,11 @@ console.log(`Sum of the Number ${a} and ${b} is ${c}` ) ;
 //6. Write a function expression that takes in a number and returns true if it's even and false if it's odd.
 
 function check(number){
-    if(number%2==0) return true ;
-    return false ;
+    if(number%2==0) return true ;// if number is even
+    return false ;// if number is odd
 }
 
-let number =parseInt(prompt("Enter Number :")) ;
+let number =parseInt(prompt("Enter Number :")) ; // taking user input
 check(number) ;
 //7. All of the above questions from 3-6 needs to be done with arrow functions also
 3.let  name=()=>{
@@ -50,7 +50,7 @@ call(name) ;
 let input =prompt("Enter Number: ") ;
 let ans=(input)=>{
 
-  return (input*input) ;
+  return (input*input) ; // this function returns the square of a number
 
 }
 let ans2=ans(input) ;
@@ -62,7 +62,7 @@ console.log(`Square of the Number ${input} is ${ans2}`) ;
 let a=parseInt(prompt("Enter First Number: "));
 let b=parseInt(prompt("Enter Second Number: "));
 let c =(a,b)=>{
-    return a+b ;
+    return a+b ; // return sum of number a and b 
 }
 const an = c(a,b) ;
 console.log(`Sum of the Number ${a} and ${b} is ${an}` ) ;
@@ -73,13 +73,13 @@ console.log(`Sum of the Number ${a} and ${b} is ${an}` ) ;
 
 //6.
 const check=(number)=>{
-    if(number%2==0) return true ;
-    return false ;
+    if(number%2==0) return true ; // if number is even
+    return false ;//  if number is odd
 }
 
 let number =parseInt(prompt("Enter Number :")) ;
 check(number) ;
-
+console.log(check(number)) ;
 
 
 
